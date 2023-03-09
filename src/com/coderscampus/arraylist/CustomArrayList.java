fac9920138e7cd748060a65d005737cbd9ee25fc
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-03-08 23:17:14 -0500

    Update CustomArrayList.java

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,22 +2,23 @@ package com.coderscampus.arraylist;
 
     public class CustomArrayList<T> implements CustomList<T> {
         Object[] items = new Object[3];
-        //Counting every time is adding an element to the list
+
+        //Counting the items adding to the Array
         int indexCount = 0;
 
         @Override
         public boolean add(T item) {
-            //adding elements to the list if indexCount < Array.length
+            //Adding item if Array.length < quantity of items
             if (indexCount < items.length) {
                 items[indexCount] = item;
                 indexCount++;
             }
-            //over writing the Array length if indexCount == Array.length
-            //copying the elements to the new Array
-            //Assigning the new Array to the old one
+            //Overwriting Array size if quantity of items == to Array size
+            //Creating new Array 1.8 bigger than the original and copying the old one values to the new one
+            //Assigning the new Array values to the original one
             if (indexCount == items.length) {
                 int x = 0;
-                Object[] newListItems = new Object[items.length +1];
+                Object[] newListItems = new Object[(int) Math.round(items.length *1.8)];
                 for (Object element : items) {
                     newListItems[x] = element;
                     x++;
@@ -29,13 +30,13 @@ package com.coderscampus.arraylist;
 
             @Override
             public int getSize() {
-            //the length of the array after adding all the elements
+            //Returning the resultant array size
                 return indexCount;
             }
 
             @Override
             public T get(int index){
-            //Casting to the data type use and returning specific element by index
+            //Casting to the data type use and returning the specific item by index
                 return (T)items[index];
             }
         }
