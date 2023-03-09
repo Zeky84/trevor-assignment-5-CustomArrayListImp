eaa448a1df03e821eddabfbc98023a92204571e8
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-03-08 22:21:01 -0500

    last update

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,14 +2,19 @@ package com.coderscampus.arraylist;
 
     public class CustomArrayList<T> implements CustomList<T> {
         Object[] items = new Object[3];
+        //Counting every time is adding an element to the list
         int indexCount = 0;
 
         @Override
         public boolean add(T item) {
+            //adding elements to the list if indexCount < Array.length
             if (indexCount < items.length) {
                 items[indexCount] = item;
                 indexCount++;
             }
+            //over writing the Array length if indexCount == Array.length
+            //copying the elements to the new Array
+            //Assigning the new Array to the old one
             if (indexCount == items.length) {
                 int x = 0;
                 Object[] newListItems = new Object[items.length +1];
@@ -24,11 +29,13 @@ package com.coderscampus.arraylist;
 
             @Override
             public int getSize() {
+            //the length of the array after adding all the elements
                 return indexCount;
             }
 
             @Override
             public T get(int index){
+            //Casting to the data type use and returning specific element by index
                 return (T)items[index];
             }
         }
